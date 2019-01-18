65e17e480e5f0ff641a870be694df6e065df4467
T A <arvta@paypal.com>
2019-01-18 13:56:06 +0530

    webhook apm changes

--- app.js
+++ app.js
@@ -40,7 +40,7 @@ app.post("/webhook-apm", function(req,res,next){
   var body = req.body;
   body.date = new Date();
 
-  firebase.database().ref('/webhookAPM').push({payload: body});
+  firebase.database().ref('/webhookAPM').child("payloads").push({[body.id]: body});
   console.log("stored ")
 
 })
